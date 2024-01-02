beec62974970b0603c1ad62192f3197b0044d80b
jiwon317 <chlghqlt12@naver.com>
2024-01-03 00:56:31 +0900

    반복문 활용연습

---
+++ src/week02/W17.java
@@ -0,0 +1,21 @@
+package week02;
+
+import java.util.Scanner;
+
+public class W17 {
+    public static void main(String[] args) {
+        // 입력받는 단을 제외하고 출력!!
+        Scanner sc = new Scanner(System.in);
+        int passNum = sc.nextInt(); // 출력을 제외할 구구단수 값
+
+        // 구구단!!
+        for (int i = 2; i <= 9; i++) { //구구단의 첫 번째 수
+            if(i == passNum) {
+                continue;
+            }
+            for (int j = 1; j <= 9; j++) { // 구구단의 두 번째 수
+                System.out.println(i + "곱하기 " + j + "는 " + (i*j) + "입니다.");
+            }
+        }
+    }
+}
