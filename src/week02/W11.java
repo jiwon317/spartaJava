fd80593aff2cd21921f59f25ba1279f25ead047c
jiwon317 <chlghqlt12@naver.com>
2024-01-02 20:31:24 +0900

    조건문if

---
+++ src/week02/W11.java
@@ -0,0 +1,25 @@
+package week02;
+
+public class W11 {
+    public static void main(String[] args) {
+        boolean flag = false;
+
+//        if(1 != 1) {
+//            //true인 경우
+//            System.out.println("값이 true 입니다.");
+//        } else {
+//            //false인 경우
+//            System.out.println("값이 false 입니다.");
+//        }
+        // 조건문
+        int number = 1;
+
+        if(number == 999) {
+            System.out.println("number의 값은 1입니다.");
+        } else if (number == 2) {
+            System.out.println("number의 값은 2입니다.");
+        } else {
+            System.out.println("number값은 모르는 값입니다.");
+        }
+    }
+}
