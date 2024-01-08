fd1526fc14d7d5a9cabe8f083b1ace767980c615
jiwon317 <chlghqlt12@naver.com>
2024-01-08 09:53:11 +0900

    객체 생성

--- src/week03/Main.java
+++ src/week03/Main.java
@@ -2,6 +2,23 @@ package week03;
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        Car[] carArray = new Car[3];
+        car1.changeGear('P');
+        carArray[0] = car1;
 
+        Car car2 = new Car();
+        car2.changeGear('N');
+        carArray[1] = car2;
+
+        Car car3 = new Car();
+        car3.changeGear('D');
+        carArray[2] = car3;
+
+        for (Car car: carArray) {
+            System.out.println("car.gear = " + car.gear);
+        }
+
+        System.out.println(car1); // week03.Car@5acf9800
     }
 }
