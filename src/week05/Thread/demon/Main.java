e362155876b0c0b2fe877668b0c61b6ca9fcd047
jiwon317 <chlghqlt12@naver.com>
2024-01-17 12:46:14 +0900

    데몬,우선순위,쓰레드그룹

---
+++ src/week05/Thread/demon/Main.java
@@ -0,0 +1,22 @@
+package week05.Thread.demon;
+
+public class Main {
+    public static void main(String[] args) {
+        Runnable demon = () -> {
+            for (int i = 0; i < 1000000; i++) {
+                System.out.println(i+"번쨰 demon");
+            }
+        };
+
+
+        // 우선순위가 낮다 ! -> 상대적으로 다른 쓰레드에 비해 리소스를 적게 할당 받음.
+        Thread thread = new Thread(demon);
+        thread.setDaemon(true); // true로 설정시 데몬스레드로 실행됨
+
+        thread.start();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("task");
+        }
+    }
+}
